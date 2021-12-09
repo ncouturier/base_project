ce44981a5c290332f894d4bb897ea3d7a0a95859
ncouturier <ncouturier@users.noreply.github.com>
2021-12-09 17:18:03 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="ncouturier"
+original_name="base_project"
+original_urlname="base_project"
+original_description="Awesome base_project created by ncouturier"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv base_project $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
--- base_project/base.py
+++ base_project/base.py
@@ -1,7 +1,7 @@
 """
-project_name base module.
+base_project base module.
 
-This is the principal module of the project_name project.
+This is the principal module of the base_project project.
 here you put your main classes and objects.
 
 Be creative! do whatever you want!
